24894112b24b96c15103782b46aeff6c03671160
Coba0405 <138278415+Coba0405@users.noreply.github.com>
2023-09-08 10:48:17 +0000

    各モデル作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    homes_about_path
+  end
+  # ログイン、新規登録した後に最初にどこに遷移させるかを設定する記述
+  # 引数の(resource)はログインを実行したモデルのデータのインスタンスが格納される
+  
+  def after_sign_out_path_for(resource)
+    homes_about_path
+  end  
+  # ログアウトした後にどこに遷移させるかを設定する記述
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about"
+
   # ユーザーサイド
-  namespace :public do
-    resources :comments, only: [:create, :destroy, :edit, :update]
-    resources :posts, only: [:index, :update, :destroy, :show, :new, :create]
-    resources :likes, only: [:create, :index, :destroy]
-    resources :users, only: [:show, :edit, :update]
-    resources :registration, [:new, :create]
-    resources :sessions, [:new, :create, :destroy]
-    root to: "homes#top"
-    get "homes/about" => "homes#about"
-  end
+  # namespace :public do
+  #   resources :comments, only: [:create, :destroy, :edit, :update]
+  #   resources :posts, only: [:index, :update, :destroy, :show, :new, :create]
+  #   resources :likes, only: [:create, :index, :destroy]
+  #   resources :users, only: [:show, :edit, :update]
+  #   resources :registration, [:new, :create]
+  #   resources :sessions, [:new, :create, :destroy]
+  # end
   
   # アドミンサイド
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
