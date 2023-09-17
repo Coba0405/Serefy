a04d2f301a61d90d04405ec669d479e332701a9a
Coba0405 <138278415+Coba0405@users.noreply.github.com>
2023-09-17 14:05:57 +0000

    コメント機能、いいね機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   
+  
   has_one_attached :profile_image
   # has_one_attached :profile_image do |attachable|
     
